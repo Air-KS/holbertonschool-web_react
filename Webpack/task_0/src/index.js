d31f217a5290fede0b68c939f31039d37e8cd32c
Air-KS <kevinrogeret@gmail.com>
2024-04-15 18:17:34 +0200

    Corrig paragraph

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 
 $(document).ready(function() {
-  $('body').append('<p>Paragraphe 1</p>');
-  $('body').append('<p>Paragraphe 2</p>');
-  $('body').append('<p>Paragraphe 3</p>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 });
