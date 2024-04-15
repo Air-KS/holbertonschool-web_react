c7125a467a44ce47d1bf52dc72cb7f8070289026
Air-KS <kevinrogeret@gmail.com>
2024-04-15 18:07:15 +0200

    first commit

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+
+$(document).ready(function() {
+  $('body').append('<p>Paragraphe 1</p>');
+  $('body').append('<p>Paragraphe 2</p>');
+  $('body').append('<p>Paragraphe 3</p>');
+});
